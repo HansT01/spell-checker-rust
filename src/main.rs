1c992f452f4ef3e2f0ec1632809dc6b6231358f8
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-01-30 00:27:20 +1000

    top_k_matches function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use std::{collections::BinaryHeap, fs::read_to_string, time::Instant};
+use std::{
+    collections::{BinaryHeap, HashMap},
+    fs::read_to_string,
+    time::Instant,
+};
 
 use spell_checker_rust::Array2D;
+
+const LEN_DIFF_LIMIT: usize = 2;
 
 fn calculate_distance(s1: &str, s2: &str) -> usize {
     if s1.len().abs_diff(s2.len()) > 2 {
@@ -30,26 +36,41 @@ fn calculate_distance(s1: &str, s2: &str) -> usize {
     return matrix[rows - 1][cols - 1];
 }
 
+fn top_k_matches<'a>(
+    word_to_check: &'a str,
+    word_map: &'a HashMap<usize, Vec<&str>>,
+    k: usize,
+) -> Vec<(usize, &'a str)> {
+    let mut top_k_heap: BinaryHeap<(usize, &str)> = BinaryHeap::new();
+    for i in word_to_check.len() - LEN_DIFF_LIMIT..word_to_check.len() + LEN_DIFF_LIMIT {
+        for &word in word_map.get(&i).unwrap_or(&Vec::new()).iter() {
+            let distance = calculate_distance(word_to_check, word);
+            top_k_heap.push((distance, word));
+            if top_k_heap.len() > k {
+                top_k_heap.pop();
+            }
+        }
+    }
+    let mut matches: Vec<(usize, &str)> = top_k_heap.into();
+    matches.sort();
+    matches
+}
+
 fn main() {
     let file_content = read_to_string("data/words.txt").expect("Unable to read words.txt");
-    let words: Vec<&str> = file_content.lines().collect();
+    let word_list: Vec<&str> = file_content.lines().collect();
+    let mut word_map: HashMap<usize, Vec<&str>> = HashMap::new();
+    for word in word_list.iter() {
+        let vec = word_map.entry(word.len()).or_insert(Vec::new());
+        vec.push(word)
+    }
 
     let word_to_check = "catalyt";
     let k = 10;
 
     let start_time = Instant::now();
-    let mut top_k_heap: BinaryHeap<(usize, &str)> = BinaryHeap::new();
-    for &word in words.iter() {
-        let distance = calculate_distance(word_to_check, word);
-        top_k_heap.push((distance, word));
-        if top_k_heap.len() > k {
-            top_k_heap.pop();
-        }
-    }
+    let matches = top_k_matches(word_to_check, &word_map, k);
     let elapsed_time = start_time.elapsed();
-
-    let mut matches: Vec<(usize, &str)> = top_k_heap.into();
-    matches.sort();
 
     println!("Results: {matches:?}");
     println!("Time elapsed: {elapsed_time:.2?}");
