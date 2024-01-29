c2d26bcc45eb71c17d168971443770766b3b4dc5
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-01-29 20:05:40 +1000

    new_with_default, renamed rows and cols to num_rows and num_cols

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,16 @@ use std::{
 #[derive(Debug)]
 pub struct Array2D<T> {
     data: Box<[T]>,
-    _rows: usize,
-    _cols: usize,
+    _num_rows: usize,
+    _num_cols: usize,
 }
 
 impl<T> Array2D<T> {
-    pub fn new(rows: usize, cols: usize) -> Self
+    pub fn new(num_rows: usize, num_cols: usize) -> Self
     where
         T: Default,
     {
-        let size: usize = rows * cols;
+        let size: usize = num_rows * num_cols;
         let layout = Layout::array::<T>(size).unwrap();
         let ptr = unsafe { alloc(layout) as *mut T };
         for i in 0..size {
@@ -25,16 +25,35 @@ impl<T> Array2D<T> {
         }
         Self {
             data: unsafe { Box::from_raw(std::slice::from_raw_parts_mut(ptr, size)) },
-            _rows: rows,
-            _cols: cols,
+            _num_rows: num_rows,
+            _num_cols: num_cols,
         }
     }
 
-    pub fn from_slice(rows: usize, cols: usize, slice: &[T]) -> Self
+    pub fn new_with_default(num_rows: usize, num_cols: usize, default_value: T) -> Self
     where
         T: Copy,
     {
-        let expected_length = rows * cols;
+        let size: usize = num_rows * num_cols;
+        let layout = Layout::array::<T>(size).unwrap();
+        let ptr = unsafe { alloc(layout) as *mut T };
+        for i in 0..size {
+            unsafe {
+                ptr.add(i).write(default_value);
+            }
+        }
+        Self {
+            data: unsafe { Box::from_raw(std::slice::from_raw_parts_mut(ptr, size)) },
+            _num_rows: num_rows,
+            _num_cols: num_cols,
+        }
+    }
+
+    pub fn from_slice(num_rows: usize, num_cols: usize, slice: &[T]) -> Self
+    where
+        T: Copy,
+    {
+        let expected_length = num_rows * num_cols;
         assert_eq!(
             expected_length,
             slice.len(),
@@ -44,17 +63,17 @@ impl<T> Array2D<T> {
         );
         Self {
             data: slice.into(),
-            _rows: rows,
-            _cols: cols,
+            _num_rows: num_rows,
+            _num_cols: num_cols,
         }
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &[T]> {
-        self.data.chunks(self._cols)
+        self.data.chunks(self._num_cols)
     }
 
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut [T]> {
-        self.data.chunks_mut(self._cols)
+        self.data.chunks_mut(self._num_cols)
     }
 }
 
@@ -62,16 +81,16 @@ impl<T> Index<usize> for Array2D<T> {
     type Output = [T];
 
     fn index(&self, row: usize) -> &Self::Output {
-        let start = row * self._cols;
-        let end = start + self._cols;
+        let start = row * self._num_cols;
+        let end = start + self._num_cols;
         &self.data[start..end]
     }
 }
 
 impl<T> IndexMut<usize> for Array2D<T> {
     fn index_mut(&mut self, row: usize) -> &mut Self::Output {
-        let start = row * self._cols;
-        let end = start + self._cols;
+        let start = row * self._num_cols;
+        let end = start + self._num_cols;
         &mut self.data[start..end]
     }
 }
