368a1fd4ef847cc488ec0f03e4207743d18b77d8
Hans Teh <73771314+HansT01@users.noreply.github.com>
2024-01-29 13:37:30 +1000

    collect words.txt into str vec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let file_content = read_to_string("data/words.txt").expect("Unable to read words.txt");
+    let words: Vec<&str> = file_content.lines().collect();
 }
